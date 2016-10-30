f991b0fd9920c8ec30d60628ac84970860ce9899
Orly Liba <orlyle@gmail.com>
2016-10-29 23:21:19 -0700

    Updating function
    To support only reconstruction without spectral analysis

--- functions/cropScaleOutput.m
+++ functions/cropScaleOutput.m
@@ -2,8 +2,12 @@ function [linBscanResize,diffImg,compoundImg,linBscan,speckVarNorm] = cropScaleO
 
 linBscan = linBscan(viewRange,:,:);
 linBscanResize = linBscan;
-diffImg = diffImg(viewRange,:,:);
-compoundImg = compoundImg(viewRange,:,:);
+if ~isempty(diffImg)
+    diffImg = diffImg(viewRange,:,:);
+end
+if ~isempty(compoundImg)
+    compoundImg = compoundImg(viewRange,:,:);
+end
 if ~isempty(speckVarNorm)
     speckVarNorm = speckVarNorm(viewRange,:,:);
 end
@@ -11,15 +15,23 @@ end
 if trueToScale
     if pixX > pixZ
         linBscanResize = imresize(linBscan,[size(linBscan,1) size(linBscan,2)*pixX/pixZ],reSizeMethod);
-        diffImg = imresize(diffImg,[size(diffImg,1) size(diffImg,2)*pixX/pixZ],reSizeMethod);
-        compoundImg = imresize(compoundImg,[size(compoundImg,1) size(compoundImg,2)*pixX/pixZ],reSizeMethod);
+        if ~isempty(diffImg)
+            diffImg = imresize(diffImg,[size(diffImg,1) size(diffImg,2)*pixX/pixZ],reSizeMethod);
+        end
+        if ~isempty(compoundImg)
+            compoundImg = imresize(compoundImg,[size(compoundImg,1) size(compoundImg,2)*pixX/pixZ],reSizeMethod);
+        end
         if ~isempty(speckVarNorm)
             speckVarNorm = imresize(speckVarNorm,[size(speckVarNorm,1) size(speckVarNorm,2)*pixX/pixZ],reSizeMethod);
         end
     else
         linBscanResize = imresize(linBscan,[size(linBscan,1)*pixZ/pixX size(linBscan,2)],reSizeMethod);
-        diffImg = imresize(diffImg,[size(diffImg,1)*pixZ/pixX size(diffImg,2)],reSizeMethod);
-        compoundImg = imresize(compoundImg,[size(compoundImg,1)*pixZ/pixX size(compoundImg,2)],reSizeMethod);
+        if ~isempty(diffImg)
+            diffImg = imresize(diffImg,[size(diffImg,1)*pixZ/pixX size(diffImg,2)],reSizeMethod);
+        end
+        if ~isempty(compoundImg)
+            compoundImg = imresize(compoundImg,[size(compoundImg,1)*pixZ/pixX size(compoundImg,2)],reSizeMethod);
+        end
         if ~isempty(speckVarNorm)
             speckVarNorm = imresize(speckVarNorm,[size(speckVarNorm,1)*pixZ/pixX size(speckVarNorm,2)],reSizeMethod);
         end
