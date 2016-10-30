ab313cb8cc75e0997af89aaf1a8c3c99111f624f
Orly Liba <orlyle@gmail.com>
2016-10-29 20:24:51 -0700

    Delete findLocalMax.m

--- ROI%20analysis/findLocalMax.m
+++
@@ -1,8 +0,0 @@
-function [map] = findLocalMax(ROI,threshold)
-Dx = ROI(2:end-1,1:end-1) - ROI(2:end-1,2:end);
-Dy = ROI(1:end-1,2:end-1) - ROI(2:end,2:end-1);
-map = zeros(size(ROI));
-map(2:end-1,2:end-1) = (Dx(:,1:end-1).*Dx(:,2:end) < 0) & (Dy(1:end-1,:).*Dy(2:end,:) < 0) & (ROI(2:end-1,2:end-1) > threshold);
-% figure; imagesc(map); colormap gray;
-% figure; imagesc(ROI(2:end-1,2:end-1)); colormap gray;
-
